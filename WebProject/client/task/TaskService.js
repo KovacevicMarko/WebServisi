78322aa1030a4460ea003e3f2d2ab4a1e3501bb0
vladandesnica <vladan.d93@gmail.com>
2016-06-06 04:32:43 +0200

    ChangeTask promenjena da cuva verzije, a ne promene.

--- WebProject/client/task/TaskService.js
+++ WebProject/client/task/TaskService.js
@@ -4,44 +4,45 @@ angular.module('MyApp')
 		
         getTask: function(onSuccess, onError,taskId){
 		
-		var req = {
+		    var req = {
                 method : "GET",
                 url: "/TaskService/tasks/"+taskId,
                 headers: {
                      'Content-Type': "application/json"
                          }
-            }	
+             }	
 
-		$http(req).then(onSuccess, onError);
-	},
+		    $http(req).then(onSuccess, onError);
+	    },
     
-    addTask : function(onSuccess,onError,data,projectId) {
+        addTask : function(onSuccess,onError,data,projectId) {
         
-        var req = {
-            method : "POST",
-            url: "/TaskService/task/"+projectId,
-            headers: {
+            var req = {
+              method : "POST",
+               url: "/TaskService/task/"+projectId,
+              headers: {
                      'Content-Type': "application/json"
                      },
-            data : data
-        }
+              data : data
+            }
         
-        $http(req).then(onSuccess, onError);
-    },
+          $http(req).then(onSuccess, onError);
+        
+        },
     
-    changeTask : function(onSuccess,onError,data) {
-        
-        var req = {
-            method : "POST",
-            url: "/TaskService/changeTask",
-            headers: {
-                     'Content-Type': "application/json"
-                     },
-            data : data
+        changeTask : function(onSuccess,onError,data,taskId) {
+            
+            var req = {
+                method : "POST",
+                url: "/TaskService/changeTask/"+taskId,
+                headers: {
+                        'Content-Type': "application/json"
+                        },
+                data : data
+            }
+            
+            $http(req).then(onSuccess, onError);
         }
-        
-        $http(req).then(onSuccess, onError);
-    }
     
   }
 });
